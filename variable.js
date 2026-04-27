f6135795c2916e1a26de369bd340e2989114d51c
SnehaJena <sneha.jena1994@gmail.com>
2026-04-27 19:41:11 +0530

    changes made in variable

--- variable.js
+++ variable.js
@@ -1,3 +1,4 @@
+/*
 var a = 12
 console.log(a)
 let emailAddress = "sneha.jena4@gmail.com";
@@ -6,3 +7,22 @@ const personName = "Sneha Jena";
 console.log(personName);
 emailAddress = "sneha.jena1994@gmail.com";
 console.log(emailAddress);
+*/
+
+let score=85;
+if(score>=80){
+    let studentA= "Gets score B"
+    console.log(studentA);
+}
+else{
+    console.log("Get score A");
+}
+ score=90;
+if(!score>=90){
+    let studentA= "Gets score A"
+    console.log(studentA);
+}
+else{
+    console.log("Get score C");
+}
+console.log("score:" + (score>=90? "A" : score >=80 ?"B" : "C or below"));
